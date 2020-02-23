1ce089ce99e8e90adace6b1fbd4e0bbdc56fb8f6
Ndi Cedric Tifuh <tncedric@gmail.com>
2020-02-23 13:24:12 +0100

    Initialised project with laravel authentication

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    PCC Mobile Application Backend 
                 </div>
 
                 <div class="links">
