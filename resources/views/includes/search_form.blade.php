b2325fd10be0e29cd5d1f05a31ab6ac47a880960
Ndi Cedric Tifuh <tncedric@gmail.com>
2020-07-05 07:51:44 +0100

    Changed UI for the admin backend

--- resources/views/includes/search_form.blade.php
+++ resources/views/includes/search_form.blade.php
@@ -1,11 +1,9 @@
-<li class="hidden-sm hidden-xs searchform">
-    <form action="http://jaybabani.com/complete-admin/v6.1/preview/ui-search.html" method="post">
-        <div class="input-group">
-            <span class="input-group-addon">
-                <i class="fa fa-search"></i>
-            </span>
-            <input type="text" class="form-control animated fadeIn" placeholder="Search & Enter">
-        </div>
-        <input type='submit' value="">
-    </form>
-</li>
+<div class="search-bar">
+    <div class="search-icon">
+        <i class="material-icons">search</i>
+    </div>
+    <input type="text" placeholder="START TYPING...">
+    <div class="close-search">
+        <i class="material-icons">close</i>
+    </div>
+</div>
